2866dcd62e2963651b3ff206215b9e22fe46326b
duongthucamtien123 <tienb1910154@student.ctu.edu.vn>
2022-11-29 11:18:20 +0700

    client

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ],
-  devServer:{
-    proxy:"http://localhost:5000"
-  }
-})
+  transpileDependencies: ["vuetify"],
+  devServer: {
+    proxy: "http://localhost:5000",
+  },
+  // publicPath: process.env.NODE_ENV === "production" ? "/dist/" : "/",
+  // base: process.env.NODE_ENV === "production" ? "/flower/" : "/",
+});
